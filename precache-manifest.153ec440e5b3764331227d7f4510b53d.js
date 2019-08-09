b4c8a469b07be44dda2cdecf239635fb541f6e70
João Baptista <joaobat@live.com.pt>
2019-08-09 18:08:59 +0100

    Updates

---
+++ precache-manifest.153ec440e5b3764331227d7f4510b53d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f5d7264f31c6bfb4f5ceeaa038bcd6",
+    "url": "/GradientGenerator/index.html"
+  },
+  {
+    "revision": "5711c9ab4263c91f35e6",
+    "url": "/GradientGenerator/static/css/main.17b84bab.chunk.css"
+  },
+  {
+    "revision": "1e4ac21f8cbfa62afc80",
+    "url": "/GradientGenerator/static/js/2.1226d55a.chunk.js"
+  },
+  {
+    "revision": "5711c9ab4263c91f35e6",
+    "url": "/GradientGenerator/static/js/main.ee8985d9.chunk.js"
+  },
+  {
+    "revision": "c87dd6d59c92b1c5b4fa",
+    "url": "/GradientGenerator/static/js/runtime~main.4dc0928c.js"
+  }
+]);
